d92c05f98312ec6de4631a2d07b3512a3352eda9
Harry Odinson <vandet.dev@gmail.com>
2024-09-02 22:50:54 +0700

    update

--- lib/models/ott_provider.dart
+++ lib/models/ott_provider.dart
@@ -6,6 +6,17 @@ class OTTProviderModel {
     this.name = "",
     this.providers = const [],
   });
+
+  factory OTTProviderModel.fromMap(Map<String, dynamic> map) => OTTProviderModel(
+    name: map["name"] ?? "",
+    providers: List<OTTProviderItemModel>.from((map["providers"] ?? []).map((e) => OTTProviderItemModel.fromMap(e))),
+  );
+
+  Map<String, dynamic> toMap() => {
+    "name": name,
+    "providers": providers.map((e) => e.toMap()).toList(),
+  };
+
 }
 
 class OTTProviderItemModel {
@@ -23,7 +34,22 @@ class OTTProviderItemModel {
     this.presentation = "",
   });
 
+  factory OTTProviderItemModel.fromMap(Map<String, dynamic> map) => OTTProviderItemModel(
+    name: map["name"] ?? "",
+    link: map["link"] ?? "",
+    price: map["price"] ?? "",
+    image: map["image"] ?? "",
+    presentation: map["presentation"] ?? "",
+  );
 
   @override
   String toString() => "OTTProviderItemModel($name, $link, $price, $image, $presentation)";
+
+  Map<String, dynamic> toMap() => {
+    "name": name,
+    "link": link,
+    "price": price,
+    "image": image,
+    "presentation": presentation,
+  };
 }
