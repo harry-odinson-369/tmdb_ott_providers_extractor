cf4bead2afa21e0f1009273c318bd7abbb246973
Harry Odinson <vandet.dev@gmail.com>
2024-09-07 02:10:54 +0700

    update redirect link launcher

--- lib/services/launcher.dart
+++ lib/services/launcher.dart
@@ -1,37 +1,32 @@
 import 'dart:async';
+import 'dart:io';
 
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:tmdb_ott_providers_extractor/models/ott_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LauncherService {
 
   ///[directLink] is the link from [OTTProviderItemModel] link property.
-  static Future launchDirectLink(String directLink) async {
-    Completer completer = Completer();
-
+  static Future launchRedirectLink(String directLink) async {
     var uri = Uri.parse(directLink);
 
-    var hl = HeadlessInAppWebView(
-      initialUrlRequest: URLRequest(
-        url: WebUri.uri(uri),
-      ),
-      shouldOverrideUrlLoading: (controller, navigationAction) async {
-        if (navigationAction.request.url?.host != uri.host) {
-          await launchUrl(navigationAction.request.url ?? uri);
-          if (!completer.isCompleted) {
-            completer.complete();
-          }
-          return NavigationActionPolicy.CANCEL;
-        } else {
-          return NavigationActionPolicy.ALLOW;
-        }
-      },
-    )..run();
+    var client = HttpClient();
+    var request = await client.getUrl(uri);
+    request.followRedirects = false;
+    var response = await request.close();
+    while (response.isRedirect) {
+      response.drain();
+      final location = response.headers.value(HttpHeaders.locationHeader);
+      if (location != null) {
+        uri = uri.resolve(location);
+        request = await client.getUrl(uri);
+        // Set the body or headers as desired.
+        request.followRedirects = false;
+        response = await request.close();
+      }
+    }
 
-    await completer.future;
-
-    hl.dispose();
+    await launchUrl(Uri.parse(response.headers[HttpHeaders.locationHeader] as String? ?? uri.toString()));
 
     return;
   }
