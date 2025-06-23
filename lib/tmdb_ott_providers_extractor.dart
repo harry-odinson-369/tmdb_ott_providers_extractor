f0debed9d1e596586e2a55d360cba624db74db91
Harry Odinson <vandet.dev@gmail.com>
2025-06-23 14:18:58 +0700

    update

--- lib/tmdb_ott_providers_extractor.dart
+++ lib/tmdb_ott_providers_extractor.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 library tmdb_ott_providers_extractor;
 
 import 'dart:async';
@@ -11,10 +13,43 @@ import 'package:tmdb_ott_providers_extractor/constants/urls.dart';
 import 'package:tmdb_ott_providers_extractor/models/ott_provider.dart';
 import 'package:tmdb_ott_providers_extractor/services/extractor.dart';
 
-export 'package:tmdb_ott_providers_extractor/services/launcher.dart';
+import 'package:url_launcher/url_launcher.dart' show LaunchMode, launchUrl;
 
 class TMDbOTTProvidersExtractor {
-  static Future<List<OTTProviderModel>> extract(String id, String type, String locale) async {
+  /// get the last redirected link from [link] property.
+  static Future<Uri> redirect_to_last_link(String link) async {
+    var uri = Uri.parse(link);
+
+    var client = HttpClient();
+    var request = await client.getUrl(uri);
+    request.followRedirects = false;
+    var response = await request.close();
+    while (response.isRedirect) {
+      response.drain();
+      final location = response.headers.value(HttpHeaders.locationHeader);
+      if (location != null) {
+        uri = uri.resolve(location);
+        request = await client.getUrl(uri);
+        // Set the body or headers as desired.
+        request.followRedirects = false;
+        response = await request.close();
+      }
+    }
+
+    return Uri.parse(response.headers[HttpHeaders.locationHeader] as String? ??
+        uri.toString());
+  }
+
+  ///[directLink] is the url from [OTTProviderItemModel] link property.
+  static Future launchRedirectLink(String directLink,
+      [LaunchMode launchMode = LaunchMode.platformDefault]) async {
+    final uri = await redirect_to_last_link(directLink);
+    launchUrl(uri, mode: launchMode);
+    return;
+  }
+
+  static Future<List<OTTProviderModel>> extract(
+      String id, String type, String locale) async {
     Uri uri = Uri.parse(
         "${Urls.baseUrl}/$type/$id/watch?translate=false&locale=$locale");
     try {
@@ -50,7 +85,8 @@ class TMDbOTTProvidersExtractor {
         url: WebUri.uri(uri),
       ),
       onLoadStop: (controller, url) async {
-        final source = await controller.evaluateJavascript(source: "document.documentElement.outerHTML;");
+        final source = await controller.evaluateJavascript(
+            source: "document.documentElement.outerHTML;");
         if (!completer.isCompleted) {
           completer.complete(source);
         }
--- test/tmdb_ott_providers_extractor_test.dart
+++ test/tmdb_ott_providers_extractor_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+// ignore_for_file: avoid_print
 
 import 'package:tmdb_ott_providers_extractor/tmdb_ott_providers_extractor.dart';
 
